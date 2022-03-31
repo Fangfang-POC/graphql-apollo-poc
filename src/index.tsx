fa8dfbce4d4e3bab54edb24b98a8779ea3dbd01d
FangfangHao <fangfanghao@seismic.com>
2022-03-31 17:45:17 +0800

    add apollo-client to fetch data from server

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { ApolloClient, InMemoryCache, gql, ApolloProvider } from '@apollo/client';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'));
