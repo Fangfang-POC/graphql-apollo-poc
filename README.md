5ff009ea752d27713e98b3fd9c6c5ffec8c97134
FangfangHao <fangfanghao@seismic.com>
2022-04-07 15:43:47 +0800

    update README

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # graphql-apollo-poc
 [Graphql Codegen Best Practices](https://www.the-guild.dev/blog/graphql-codegen-best-practices)
 ## Local development
-yarn install
-yarn start
-
+- `yarn install`
+- `yarn start`
+- The default browser will be opened automatically to show the POC. Or you can view the demo on `http://localhost:8888`
 ### Generate types according to the schema and graphql queries and mutations
-yarn gen:types
+- `yarn gen:types`
+- view the generated types in `src/types.ts`
--- src/types.ts
+++ src/types.ts
@@ -8,161 +8,159 @@ export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Mayb
 const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-    ID: string;
-    String: string;
-    Boolean: boolean;
-    Int: number;
-    Float: number;
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
 };
 
 export type AddUserInput = {
-    age?: InputMaybe<Scalars['Int']>;
-    name?: InputMaybe<Scalars['String']>;
+  age?: InputMaybe<Scalars['Int']>;
+  name?: InputMaybe<Scalars['String']>;
 };
 
 export type Airplane = Vehicle & {
-    __typename?: 'Airplane';
-    maxSpeed?: Maybe<Scalars['Int']>;
-    wingspan?: Maybe<Scalars['Int']>;
+  __typename?: 'Airplane';
+  maxSpeed?: Maybe<Scalars['Int']>;
+  wingspan?: Maybe<Scalars['Int']>;
 };
 
 export type Car = Vehicle & {
-    __typename?: 'Car';
-    licensePlate?: Maybe<Scalars['String']>;
-    maxSpeed?: Maybe<Scalars['Int']>;
+  __typename?: 'Car';
+  licensePlate?: Maybe<Scalars['String']>;
+  maxSpeed?: Maybe<Scalars['Int']>;
 };
 
 export type Cat = {
-    __typename?: 'Cat';
-    gender?: Maybe<Scalars['String']>;
-    length?: Maybe<Scalars['Int']>;
-    name?: Maybe<Scalars['String']>;
+  __typename?: 'Cat';
+  gender?: Maybe<Scalars['String']>;
+  length?: Maybe<Scalars['Int']>;
+  name?: Maybe<Scalars['String']>;
 };
 
 export type Dog = {
-    __typename?: 'Dog';
-    age?: Maybe<Scalars['Int']>;
-    gender?: Maybe<Scalars['String']>;
-    weight?: Maybe<Scalars['Int']>;
+  __typename?: 'Dog';
+  age?: Maybe<Scalars['Int']>;
+  gender?: Maybe<Scalars['String']>;
+  weight?: Maybe<Scalars['Int']>;
 };
 
 export type Faction = Node & {
-    __typename?: 'Faction';
-    id: Scalars['ID'];
-    name?: Maybe<Scalars['String']>;
-    ships?: Maybe<ShipConnection>;
+  __typename?: 'Faction';
+  id: Scalars['ID'];
+  name?: Maybe<Scalars['String']>;
+  ships?: Maybe<ShipConnection>;
 };
 
 export type Human = {
-    __typename?: 'Human';
-    age?: Maybe<Scalars['Int']>;
-    name?: Maybe<Scalars['String']>;
+  __typename?: 'Human';
+  age?: Maybe<Scalars['Int']>;
+  name?: Maybe<Scalars['String']>;
 };
 
 export type Mutation = {
-    __typename?: 'Mutation';
-    addUser?: Maybe<User>;
+  __typename?: 'Mutation';
+  addUser?: Maybe<User>;
 };
 
+
 export type MutationAddUserArgs = {
-    input?: InputMaybe<AddUserInput>;
+  input?: InputMaybe<AddUserInput>;
 };
 
 export type Node = {
-    id: Scalars['ID'];
+  id: Scalars['ID'];
 };
 
 export type PageInfo = {
-    __typename?: 'PageInfo';
-    endCursor?: Maybe<Scalars['String']>;
-    hasNextPage: Scalars['Boolean'];
-    hasPreviousPage: Scalars['Boolean'];
-    startCursor?: Maybe<Scalars['String']>;
+  __typename?: 'PageInfo';
+  endCursor?: Maybe<Scalars['String']>;
+  hasNextPage: Scalars['Boolean'];
+  hasPreviousPage: Scalars['Boolean'];
+  startCursor?: Maybe<Scalars['String']>;
 };
 
 export type Query = {
-    __typename?: 'Query';
-    empire?: Maybe<Faction>;
-    node?: Maybe<Node>;
-    rebels?: Maybe<Faction>;
-    search?: Maybe<Array<Maybe<SearchResult>>>;
-    user?: Maybe<User>;
-    users?: Maybe<Array<Maybe<User>>>;
-    vehicles?: Maybe<Array<Maybe<Vehicle>>>;
+  __typename?: 'Query';
+  empire?: Maybe<Faction>;
+  node?: Maybe<Node>;
+  rebels?: Maybe<Faction>;
+  search?: Maybe<Array<Maybe<SearchResult>>>;
+  user?: Maybe<User>;
+  users?: Maybe<Array<Maybe<User>>>;
+  vehicles?: Maybe<Array<Maybe<Vehicle>>>;
 };
+
 
 export type QueryNodeArgs = {
-    id: Scalars['ID'];
+  id: Scalars['ID'];
 };
+
 
 export type QueryRebelsArgs = {
-    id: Scalars['ID'];
+  id: Scalars['ID'];
 };
 
+
 export type QueryUserArgs = {
-    id: Scalars['ID'];
+  id: Scalars['ID'];
 };
 
 export type SearchResult = Cat | Dog | Human;
 
 export type Ship = Node & {
-    __typename?: 'Ship';
-    id: Scalars['ID'];
-    name?: Maybe<Scalars['String']>;
+  __typename?: 'Ship';
+  id: Scalars['ID'];
+  name?: Maybe<Scalars['String']>;
 };
 
 export type ShipConnection = {
-    __typename?: 'ShipConnection';
-    edges?: Maybe<Array<Maybe<ShipEdge>>>;
-    pageInfo: PageInfo;
+  __typename?: 'ShipConnection';
+  edges?: Maybe<Array<Maybe<ShipEdge>>>;
+  pageInfo: PageInfo;
 };
 
 export type ShipEdge = {
-    __typename?: 'ShipEdge';
-    cursor: Scalars['String'];
-    node?: Maybe<Ship>;
+  __typename?: 'ShipEdge';
+  cursor: Scalars['String'];
+  node?: Maybe<Ship>;
 };
 
 export type User = {
-    __typename?: 'User';
-    age?: Maybe<Scalars['Int']>;
-    id: Scalars['ID'];
-    name?: Maybe<Scalars['String']>;
+  __typename?: 'User';
+  age?: Maybe<Scalars['Int']>;
+  id: Scalars['ID'];
+  name?: Maybe<Scalars['String']>;
 };
 
 export type Vehicle = {
-    maxSpeed?: Maybe<Scalars['Int']>;
+  maxSpeed?: Maybe<Scalars['Int']>;
 };
 
 export type AddUserMutationMutationVariables = Exact<{
-    user?: InputMaybe<AddUserInput>;
+  user?: InputMaybe<AddUserInput>;
 }>;
 
-export type AddUserMutationMutation = {
-    __typename?: 'Mutation';
-    addUser?: { __typename?: 'User'; name?: string | null; id: string; age?: number | null } | null;
-};
 
-export type UsersQueryQueryVariables = Exact<{ [key: string]: never }>;
+export type AddUserMutationMutation = { __typename?: 'Mutation', addUser?: { __typename?: 'User', name?: string | null, id: string, age?: number | null } | null };
 
-export type UsersQueryQuery = {
-    __typename?: 'Query';
-    users?: Array<{ __typename?: 'User'; name?: string | null; id: string; age?: number | null } | null> | null;
-};
+export type UsersQueryQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type UsersQueryQuery = { __typename?: 'Query', users?: Array<{ __typename?: 'User', name?: string | null, id: string, age?: number | null } | null> | null };
+
 
 export const AddUserMutationDocument = gql`
     mutation AddUserMutation($user: AddUserInput) {
-        addUser(input: $user) {
-            name
-            id
-            age
-        }
-    }
-`;
-export type AddUserMutationMutationFn = Apollo.MutationFunction<
-    AddUserMutationMutation,
-    AddUserMutationMutationVariables
->;
+  addUser(input: $user) {
+    name
+    id
+    age
+  }
+}
+    `;
+export type AddUserMutationMutationFn = Apollo.MutationFunction<AddUserMutationMutation, AddUserMutationMutationVariables>;
 
 /**
  * __useAddUserMutationMutation__
@@ -181,30 +179,22 @@ export type AddUserMutationMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useAddUserMutationMutation(
-    baseOptions?: Apollo.MutationHookOptions<AddUserMutationMutation, AddUserMutationMutationVariables>,
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useMutation<AddUserMutationMutation, AddUserMutationMutationVariables>(
-        AddUserMutationDocument,
-        options,
-    );
-}
+export function useAddUserMutationMutation(baseOptions?: Apollo.MutationHookOptions<AddUserMutationMutation, AddUserMutationMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<AddUserMutationMutation, AddUserMutationMutationVariables>(AddUserMutationDocument, options);
+      }
 export type AddUserMutationMutationHookResult = ReturnType<typeof useAddUserMutationMutation>;
 export type AddUserMutationMutationResult = Apollo.MutationResult<AddUserMutationMutation>;
-export type AddUserMutationMutationOptions = Apollo.BaseMutationOptions<
-    AddUserMutationMutation,
-    AddUserMutationMutationVariables
->;
+export type AddUserMutationMutationOptions = Apollo.BaseMutationOptions<AddUserMutationMutation, AddUserMutationMutationVariables>;
 export const UsersQueryDocument = gql`
     query UsersQuery {
-        users {
-            name
-            id
-            age
-        }
-    }
-`;
+  users {
+    name
+    id
+    age
+  }
+}
+    `;
 
 /**
  * __useUsersQueryQuery__
@@ -222,15 +212,13 @@ export const UsersQueryDocument = gql`
  * });
  */
 export function useUsersQueryQuery(baseOptions?: Apollo.QueryHookOptions<UsersQueryQuery, UsersQueryQueryVariables>) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useQuery<UsersQueryQuery, UsersQueryQueryVariables>(UsersQueryDocument, options);
-}
-export function useUsersQueryLazyQuery(
-    baseOptions?: Apollo.LazyQueryHookOptions<UsersQueryQuery, UsersQueryQueryVariables>,
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useLazyQuery<UsersQueryQuery, UsersQueryQueryVariables>(UsersQueryDocument, options);
-}
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<UsersQueryQuery, UsersQueryQueryVariables>(UsersQueryDocument, options);
+      }
+export function useUsersQueryLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<UsersQueryQuery, UsersQueryQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<UsersQueryQuery, UsersQueryQueryVariables>(UsersQueryDocument, options);
+        }
 export type UsersQueryQueryHookResult = ReturnType<typeof useUsersQueryQuery>;
 export type UsersQueryLazyQueryHookResult = ReturnType<typeof useUsersQueryLazyQuery>;
 export type UsersQueryQueryResult = Apollo.QueryResult<UsersQueryQuery, UsersQueryQueryVariables>;
